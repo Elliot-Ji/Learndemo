73d84f00950ce316c5065c9382de02ab8154eaa1
ElliotJi <1047810944@qq.com>
2020-08-03 21:16:10 +0800

    顺序存储二叉树

---
+++ src/main/java/com/example/dataStructure/tree/ArrBinaryTreeDemo.java
@@ -0,0 +1,40 @@
+package com.example.dataStructure.tree;
+
+/**
+ * 顺序存储二叉树:
+ *       树可以转成数组
+ *      b站: p101
+ * @author Elliot Ji
+ * @date 2020/8/3
+ * @time 21:05
+ */
+public class ArrBinaryTreeDemo {
+
+    public static void main(String[] args) {
+
+    }
+
+}
+
+//顺序存储二叉树
+class ArrBinaryTree{
+
+    //顺序存储二叉树的前序遍历
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
